3a1b7152758b52a7b2730b5255d223f3c098762b
Mohnish Thallavajhula <i@mohni.sh>
2013-08-12 20:27:43 -0700

    half assed parallax

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+
 (function(window, document) {
-  // ..
+  var parallaxElement = document.querySelector('#parallax')
+    , scrollInfo = document.querySelector('.scroll');
+
+  var parallaxScroll = function(e) {
+    // Uses CPU
+    // parallaxElement.style.marginTop = (window.scrollY/2) + 'px'
+
+    // Uses GPU
+    parallaxElement.style.transform = 'translate3d(0px, ' + (window.scrollY/2) + 'px, 0px)'
+
+    scrollInfo.innerHTML = 'Actual: ' + window.scrollY + '; Parallax: ' + (window.scrollY/2)
+  }
+
+  window.onscroll = parallaxScroll;
+
 })(window, document);
